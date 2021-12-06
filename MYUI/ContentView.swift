1d5c5e0aa0e4015f964f9479a58eb250492a745d
Daniyal Khan <dk720820@gmail.com>
2021-12-06 18:09:37 +0500

    Initial commit

---
+++ MYUI/ContentView.swift
@@ -0,0 +1,157 @@
+//
+//  ContentView.swift
+//  MYUI
+//
+//  Created by JOEYCO-0072PK on 06/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isNight = false
+    var body: some View {
+        
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [isNight ?.black :.blue,isNight ?.gray:.white]), startPoint: .leading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            VStack(spacing:20){
+                Text("Pakistan, Karachi")
+                    .padding()
+                    .font(.system(size: 28,weight: .bold))
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.bottom)
+                VStack(spacing:8){
+                    
+                    Image("icon2")
+                        .renderingMode(.original)
+                        .frame(width: 160, height: 160)
+                    Text("76°")
+                        .font(.system(size: 68,weight: .bold))
+                        .foregroundColor(.white)
+                        .padding(.leading)
+                        .padding(.leading)
+                    
+                    
+                }
+                Spacer()
+                HStack(spacing:2){
+                    VStack{
+                        Text("MON")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                        Image("icon2")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        Text("56°")
+                            .font(.system(size: 20,weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                        
+                        
+                    }
+                    VStack{
+                        Text("TUE")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                        Image("icon3")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        Text("36°")
+                            .font(.system(size: 20,weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                        
+                        
+                    }
+                    VStack{
+                        Text("WED")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                        Image("icon2")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        Text("60°")
+                            .font(.system(size: 20,weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                        
+                        
+                    }
+                    VStack{
+                        Text("THU")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                        Image("icon3")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        Text("32°")
+                            .font(.system(size: 20,weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                        
+                        
+                    }
+                    VStack{
+                        Text("FRI")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                        Image("icon2")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        Text("72°")
+                            .font(.system(size: 20,weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                        
+                        
+                    }
+                    VStack{
+                        Text("SAT")
+                            .font(.system(size: 18))
+                            .foregroundColor(.white)
+                        Image("icon3")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                        Text("24°")
+                            .font(.system(size: 20,weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(.leading)
+                        
+                        
+                    }
+                    
+                    
+                    
+                }
+                Spacer()
+                Button{
+                    isNight.toggle()
+                }label:{
+                    Text("Change Theme")
+                        .frame(width: 200, height: 60)
+                        .background(.white)
+                        .font(.system(size: 18,weight: .bold))
+                        .foregroundColor(.blue)
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }
+            
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MYUI/MYUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYUIApp.swift
+//  MYUI
+//
+//  Created by JOEYCO-0072PK on 06/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MYUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
